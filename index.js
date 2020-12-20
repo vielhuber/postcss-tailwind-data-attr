b3d50d2173154fba6e2bfb6322e8317ed32ae6a5
David Vielhuber <david@vielhuber.de>
2020-12-20 23:40:32 +0100

    Update to postcss 8

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-let postcss = require('postcss')
-
-module.exports = postcss.plugin('postcss-tailwind-data-attr', () => {
-  return root => {
-    root.walkRules(rule => {
-      if (rule.selector.includes('tw-')) {
-        rule.selector = rule.selector
-          .split(' ')
-          .map(j => j
-            .split('.')
-            .map(i => {
-              if (i === '') {
-                return ''
-              }
-              if (i.includes('tw-')) {
-                return '[data-tw~="' + i.split('tw-').join('') + '"]'
-              }
-              return i
-            })
-            .join('')
-          )
-          .join(' ')
-      }
-    })
+module.exports = () => {
+  return {
+    postcssPlugin: 'postcss-tailwind-data-attr',
+    Root (root) {
+      root.walkRules(rule => {
+        if (rule.selector.includes('tw-')) {
+          rule.selector = rule.selector
+            .split(' ')
+            .map(j =>
+              j
+                .split('.')
+                .map(i => {
+                  if (i === '') {
+                    return ''
+                  }
+                  if (i.includes('tw-')) {
+                    return '[data-tw~="' + i.split('tw-').join('') + '"]'
+                  }
+                  return i
+                })
+                .join('')
+            )
+            .join(' ')
+        }
+      })
+    }
   }
-})
+}
+module.exports.postcss = true
